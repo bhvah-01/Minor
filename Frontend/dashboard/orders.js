49c9d192f5495602335e3745acc8c2196e475b14
bhvah-01 <92270353+bhvah-01@users.noreply.github.com>
2023-11-01 11:13:29 +0530

    Create orders.js

---
+++ Frontend/dashboard/orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        Name: 'Ritika',
+        Mail: 'abc@gmail.com',
+        paymentStatus: 'Due',
+       
+    },
+    {
+        Name: 'Ashlesha',
+        Mail: 'pqr@gmail.com',
+        paymentStatus: 'Refunded',
+        
+    },
+    {
+        Name: 'Bhvah Shree',
+        Mail: 'xyz@gmail.com',
+        paymentStatus: 'Paid',
+
+    },
+]
